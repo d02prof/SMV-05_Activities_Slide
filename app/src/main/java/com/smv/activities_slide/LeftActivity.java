2a3619f01020e2ba7faaf1dccbade4fc8179fda5
Boštjan Resinovič <bostjan.resinovic@gmail.com>
2020-02-21 10:12:44 +0100

    Dela

---
+++ app/src/main/java/com/smv/activities_slide/LeftActivity.java
@@ -0,0 +1,46 @@
+package com.smv.activities_slide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class LeftActivity extends AppCompatActivity
+{
+    //y ne rabimo, seveda pa obstaja
+    float x1, x2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_left);
+
+        setTitle("Left Activity");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        //return super.onTouchEvent(event);
+
+        //x1 in x2 sta definirana v razredu, če sta lokalni spremenljivki, ne dela ok
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                Intent intent;
+                if(x1 > x2)
+                {
+                    intent = new Intent(LeftActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                break;
+        }
+        return true;
+    }
+}
